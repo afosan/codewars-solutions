14621730dd6e2a7bc72b508f17c3146f02c09578
afosan <afosan@skiff.com>
2024-02-06 21:25:59 +0300

    7: 1

---
+++ rust/7kyu/number-of-people-in-the-bus/src/lib.rs
@@ -0,0 +1,17 @@
+//! https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/rust
+
+pub fn number(bus_stops:&[(i32, i32)]) -> i32 {
+    bus_stops.iter().map(|(ins, outs)| ins - outs).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(number(&[(10,0),(3,5),(5,8)]), 5);
+        assert_eq!(number(&[(3,0),(9,1),(4,10),(12,2),(6,1),(7,10)]), 17);
+        assert_eq!(number(&[(3,0),(9,1),(4,8),(12,2),(6,1),(7,8)]), 21);
+    }
+}
