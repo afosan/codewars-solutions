4d81a8027838b6526f09d428af9bf43acf940791
afosan <afosan@skiff.com>
2024-02-24 18:22:09 +0300

    fix

--- rust/7kyu/dont-give-me-five/src/lib.rs
+++ rust/7kyu/dont-give-me-five/src/lib.rs
@@ -1,7 +1,24 @@
 //! https://www.codewars.com/kata/5813d19765d81c592200001a/train/rust
 
+fn to_digits(n: isize) -> Vec<u8> {
+    let mut n = n.abs() as u64;
+
+    if n == 0 {
+        vec![0]
+    } else {
+        let mut out = vec![];
+
+        while n > 0 {
+            out.push((n % 10).try_into().unwrap());
+            n /= 10;
+        }
+
+        out
+    }
+}
+
 pub fn dont_give_me_five(start: isize, end: isize) -> isize {
-    (start..=end).filter(|n| n % 10 != 5).count() as isize
+    (start..=end).filter(|&n| to_digits(n).into_iter().find(|&d| d == 5).is_none()).count() as isize
 }
 
 #[cfg(test)]
