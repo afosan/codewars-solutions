4090f50486a2da046f52bad9e270af08376fa60d
afosan <afosan@skiff.com>
2024-02-27 22:38:10 +0300

    fix

--- rust/7kyu/cats-and-shelves/src/lib.rs
+++ rust/7kyu/cats-and-shelves/src/lib.rs
@@ -1,8 +1,8 @@
 //! https://www.codewars.com/kata/62c93765cef6f10030dfa92b/train/rust
 
 pub fn cats_and_shelves(start: u8, finish: u8) -> u8 {
-    let jumps = finish - start;
-    jumps / 3 + jumps % 3
+    let diff = finish - start;
+    diff / 3 + diff % 3
 }
 
 #[cfg(test)]
