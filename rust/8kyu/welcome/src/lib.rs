7f695309e04fde66fb1e618d03ad223f4ca49e9e
afosan <afosan@skiff.com>
2024-03-02 20:34:01 +0300

    8: 1

---
+++ rust/8kyu/welcome/src/lib.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+pub fn greet(language: &str) -> &str {
+    let hm = HashMap::<&str, &str>::from_iter(
+        [ ("english", "Welcome")
+        , ("czech", "Vitejte")
+        , ("danish", "Velkomst")
+        , ("dutch", "Welkom")
+        , ("estonian", "Tere tulemast")
+        , ("finnish", "Tervetuloa")
+        , ("flemish", "Welgekomen")
+        , ("french", "Bienvenue")
+        , ("german", "Willkommen")
+        , ("irish", "Failte")
+        , ("italian", "Benvenuto")
+        , ("latvian", "Gaidits")
+        , ("lithuanian", "Laukiamas")
+        , ("polish", "Witamy")
+        , ("spanish", "Bienvenido")
+        , ("swedish", "Valkommen")
+        , ("welsh", "Croeso")
+        ].into_iter()
+    );
+
+    hm.get(language).unwrap_or_else(|| &"Welcome")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fixed() {
+        assert_eq!(greet("english"), "Welcome");
+        assert_eq!(greet("dutch"), "Welkom");
+        assert_eq!(greet("IP_ADDRESS_INVALID"), "Welcome");
+        assert_eq!(greet(""), "Welcome");
+        assert_eq!(greet("swelsh"), "Welcome");
+    }
+}
