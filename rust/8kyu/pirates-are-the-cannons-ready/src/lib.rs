61963a9074c69582ad37595042282721b5699f6f
afosan <afosan@skiff.com>
2024-03-03 17:44:27 +0300

    8: 1

---
+++ rust/8kyu/pirates-are-the-cannons-ready/src/lib.rs
@@ -0,0 +1,40 @@
+//! https://www.codewars.com/kata/5748a883eb737cab000022a6/train/rust
+
+use std::collections::HashMap;
+
+pub fn cannons_ready(gunners: HashMap<&str, &str>) -> String {
+    let ready = [
+        "Mike",
+        "Joe",
+        "Johnson",
+        "Peter"
+    ].iter().all(|w| gunners.get(w) == Some(&"aye"));
+    
+    (if ready { "Fire!" } else { "Shiver me timbers!" }).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::{cannons_ready};
+    
+    #[test]
+    fn test_should_fire_the_cannons_when_ready() {
+        assert_eq!(cannons_ready(HashMap::from([
+            ("Mike", "aye"),
+            ("Joe", "aye"),
+            ("Johnson", "aye"),
+            ("Peter", "aye"),
+            ])), "Fire!");
+    }
+    
+    #[test]
+    fn test_should_shiver_your_timbers_instead_if_not_ready() {
+        assert_eq!(cannons_ready(HashMap::from([
+            ("Mike", "aye"),
+            ("Joe", "nay"),
+            ("Johnson", "aye"),
+            ("Peter", "aye"),
+            ])), "Shiver me timbers!");
+    }
+}
