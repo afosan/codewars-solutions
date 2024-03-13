f86422783eef9bf97d598f598eb9e8585b94ccc8
afosan <afosan@skiff.com>
2024-03-13 23:49:35 +0300

    fix

--- rust/6kyu/custom-fizzbuzz-array/src/lib.rs
+++ rust/6kyu/custom-fizzbuzz-array/src/lib.rs
@@ -1,14 +1,5 @@
 //! https://www.codewars.com/kata/5355a811a93a501adf000ab7/train/rust
 
-pub fn fizz_buzz_custom_solver(string_one: &str, string_two: &str, num_one: usize, num_two: usize) -> Vec<String> {
-    (1..=100).map(
-        |i|
-        if i % num_one == 0 { string_one.to_owned() }
-        else if i % num_two == 0 { string_two.to_owned() }
-        else { format!("{i}") 
-    }).collect()
-}
-
 #[macro_export]
 macro_rules! fizz_buzz_custom {
     () => { fizz_buzz_custom_solver("Fizz", "Buzz", 3, 5) };
@@ -16,6 +7,15 @@ macro_rules! fizz_buzz_custom {
     ($str_one:expr, $str_two:expr) => { fizz_buzz_custom_solver($str_one, $str_two, 3, 5) };
     ($str_one:expr, $str_two:expr, $num_one:expr) => { fizz_buzz_custom_solver($str_one, $str_two, $num_one, 5) };
     ($str_one:expr, $str_two:expr, $num_one:expr, $num_two:expr) => { fizz_buzz_custom_solver($str_one, $str_two, $num_one, $num_two) };
+}
+
+pub fn fizz_buzz_custom_solver(string_one: &str, string_two: &str, num_one: usize, num_two: usize) -> Vec<String> {
+    (1..=100).map(
+        |i|
+        if i % num_one == 0 { string_one.to_owned() }
+        else if i % num_two == 0 { string_two.to_owned() }
+        else { format!("{i}") 
+    }).collect()
 }
 
 #[cfg(test)]
